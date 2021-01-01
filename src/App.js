bb662fca83e7ac2c9d4665812338654968e61332
DoniVoice <www.bagzbanny@mail.ru>
2021-01-01 20:23:59 +0500

    chanhe index.html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Techno from './techno';
+import Header from './header';
 
-function App() {
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gr">
+      <div className='item1'></div>
+      
+      <div className='item3'></div>
+      <div className='item4'></div>
+      <div className='item5'></div>
+      <div className='item6'></div>
+      <div className='item7'></div>
+      <div className='item8'></div>
     </div>
   );
 }
+
 
 export default App;
